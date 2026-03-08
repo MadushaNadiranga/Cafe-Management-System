ffaf76d37c902e386b4178361ce905f63c22f70b
MadushaNadiranga <lmmadushanadiranga@gmail.com>
2026-03-08 23:51:09 +0530

    Updated: Added Forgot Password Function.

--- src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.inn.cafe.serviceImpl;
 
+import com.google.common.base.Strings;
 import com.inn.cafe.JWT.CustomerUserDetailsService;
 import com.inn.cafe.JWT.JwtFilter;
 import com.inn.cafe.JWT.JwtUtil;
@@ -215,6 +216,19 @@ public class UserServiceImpl implements UserService {
         }catch (Exception ex){
             ex.printStackTrace();
 
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> forgotPassword(Map<String, String> requestMap) {
+        try {
+            User user = userDao.findByEmail(requestMap.get("email"));
+            if (!Objects.isNull(user) && !Strings.isNullOrEmpty(user.getEmail()))
+                emailUtils.forgotMail(user.getEmail(),"Credentials by Cafe Management System", user.getPassword());
+                return CafeUtils.getResponseEntity("Check Your Email for Credentials", HttpStatus.OK);
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
     }
