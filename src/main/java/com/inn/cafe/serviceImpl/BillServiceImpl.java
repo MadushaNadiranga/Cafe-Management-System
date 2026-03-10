c223698d21de0a09617022e8be1561167e17df53
MadushaNadiranga <lmmadushanadiranga@gmail.com>
2026-03-11 00:35:49 +0530

    Updated: Added API of Bill Function.

--- src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
@@ -10,6 +10,7 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.pdfbox.io.IOUtils;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,8 +20,14 @@ import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Service
@@ -38,7 +45,7 @@ public class BillServiceImpl implements BillService {
     public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
         try {
             String fileName;
-            if (ValidateRequestMap(requestMap)) {
+            if (validateRequestMap(requestMap)) {
 
                 if (requestMap.containsKey("isGenerate") && !(Boolean) requestMap.get("isGenerate"))
                     fileName = (String) requestMap.get("uuid");
@@ -169,7 +176,7 @@ public class BillServiceImpl implements BillService {
         document.add(rect);
     }
 
-    private boolean ValidateRequestMap(Map<String, Object> requestMap) {
+    private boolean validateRequestMap(Map<String, Object> requestMap) {
         return requestMap.containsKey("name") &&
                 requestMap.containsKey("contactNumber") &&
                 requestMap.containsKey("email") &&
@@ -194,5 +201,67 @@ public class BillServiceImpl implements BillService {
         }catch (Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    @Override
+    public ResponseEntity<List<Bill>> getBills() {
+        List<Bill> list = new ArrayList<>();
+        if (jwtFilter.isAdmin()){
+            list = billDao.getAllBills();
+        }else {
+            list = billDao.getBillByUserName(jwtFilter.getCurrentUser());
+        }
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap) {
+        log.info("Inside getPdf : requestMap {}", requestMap);
+        try {
+            byte[] byteArray = new byte[0];
+            if (!requestMap.containsKey("uuid") && validateRequestMap(requestMap))
+                return new ResponseEntity<>(byteArray,HttpStatus.BAD_REQUEST);
+            String filePath = CafeConstants.STORE_LOCATION+"\\"+(String) requestMap.get("uuid")+".pdf";
+
+            if (CafeUtils.isFileExist(filePath)){
+                byteArray = getByteArray(filePath);
+                return new ResponseEntity<>(byteArray, HttpStatus.OK);
+            } else {
+                requestMap.put("isGenarate", false);
+                generateReport(requestMap);
+                byteArray = getByteArray(filePath);
+                return new ResponseEntity<>(byteArray, HttpStatus.OK);
+            }
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private byte[] getByteArray(String filePath) throws Exception{
+        File initialFile = new File(filePath);
+        InputStream targetStream = new FileInputStream(initialFile);
+        byte[] byteArray = IOUtils.toByteArray(targetStream);
+        targetStream.close();
+        return byteArray;
+    }
+
+    @Override
+    public ResponseEntity<String> deleteBill(Integer id) {
+        try {
+            Optional optional = billDao.findById(id);
+            if (!optional.isEmpty()){
+                billDao.deleteById(id);
+                return CafeUtils.getResponseEntity("Bll Deleted Successfully.",HttpStatus.OK);
+
+
+            }
+            return CafeUtils.getResponseEntity("Bll id does not exist.",HttpStatus.OK);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
